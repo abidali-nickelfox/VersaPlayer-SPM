7ebad3b6da870fee82e69f0f4e64e72d36a9ea9e
abidali-nickelfox <abidali.mustafa@nickelfox.com>
2025-06-17 10:39:44 +0530

    Converted VersaPlayer to Swift Package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "VersaPlayer",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "VersaPlayer",
+            targets: ["VersaPlayer"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "VersaPlayer",
+            path: "Sources/VersaPlayer/Classes"
+        )
+    ]
+)
+
